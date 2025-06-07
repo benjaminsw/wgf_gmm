418b3cabaf6ff033221b8fd0fa7d8df762f12551
Benjamin W. <benjaminsw@users.noreply.github.com>
2025-06-07 11:33:24 +0100

    Delete sec_5.3 directory

--- sec_5.3/run.sh
+++
@@ -1,4 +0,0 @@
-python scripts/sec_5.3/runner.py waveform pvi
-python scripts/sec_5.3/runner.py waveform svi
-python scripts/sec_5.3/runner.py waveform uvi
-python scripts/sec_5.3/runner.py waveform sm
